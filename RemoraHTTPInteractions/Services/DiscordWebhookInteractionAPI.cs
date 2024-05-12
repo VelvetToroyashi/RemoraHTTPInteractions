b06587d453da0c41517250a917a17b26a4ffc3a8
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2024-05-12 03:01:30 -0400

    feat: Allow followups to be translated to HTTP responses if applicable
    This intercepts calls to CreateFollowupAsync if applicable, which allows followups to be returned as an HTTP response instead of making an API call, which reduces latency.
    
    This has the downside of requiring an API call to return the message for the return type of the method. That being said, I believe the latency benefit to be a worthwhile benefit.
    
    (This also technically(?) fixes an issue wherein creating a followup would deadlock the endpoint, as it waits on a TCS, which would never be released if CreateInteractionResponse was never called.)

--- RemoraHTTPInteractions/Services/DiscordWebhookInteractionAPI.cs
+++ RemoraHTTPInteractions/Services/DiscordWebhookInteractionAPI.cs
@@ -2,7 +2,10 @@
 using OneOf;
 using Remora.Discord.API.Abstractions.Objects;
 using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.API.Objects;
 using Remora.Discord.Caching.API;
+using Remora.Discord.Commands.Contexts;
+using Remora.Discord.Commands.Services;
 using Remora.Rest.Core;
 using Remora.Results;
 using RemoraHTTPInteractions.Extensions;
@@ -11,10 +14,11 @@ namespace RemoraHTTPInteractions.Services;
 
 public class DiscordWebhookInteractionAPI : IDiscordRestInteractionAPI
 {
+    private readonly ContextInjectionService? _contextInjector;
     private readonly IDiscordRestInteractionAPI _underlying;
     private readonly InMemoryDataStore<string, InteractionWebhookResponse> _dataService;
     
-    public DiscordWebhookInteractionAPI(IDiscordRestInteractionAPI underlying)
+    public DiscordWebhookInteractionAPI(IDiscordRestInteractionAPI underlying, ContextInjectionService? contextInjector = default)
     {
         if (underlying is CachingDiscordRestInteractionAPI)
         {
@@ -26,6 +30,7 @@ public class DiscordWebhookInteractionAPI : IDiscordRestInteractionAPI
         }
         
         _underlying = underlying;
+        _contextInjector = contextInjector;
         _dataService = InMemoryDataStore<string, InteractionWebhookResponse>.Instance;
     }
 
@@ -42,7 +47,7 @@ public class DiscordWebhookInteractionAPI : IDiscordRestInteractionAPI
         CancellationToken ct = default
     )
     {
-        var interactionResult = await _dataService.TryGetLeaseAsync(interactionToken);
+        var interactionResult = await _dataService.TryGetLeaseAsync(interactionToken, ct);
         
         if (!interactionResult.IsSuccess)
         {
@@ -54,7 +59,7 @@ public class DiscordWebhookInteractionAPI : IDiscordRestInteractionAPI
         await using var interaction = interactionResult.Entity;
         
         // Ensure that the interaction is deleted after the request is complete.
-        // It's important that this is done here becuase asynchronous disposal doesn't 
+        // It's important that this is done here because asynchronous disposal doesn't 
         // guarantee that the object will be disposed of before the caller that waits on the TCS
         // is resumed. This caused a concurrency issue in 1.0.1
         interaction.MarkForDeletion(); 
@@ -77,11 +82,11 @@ public class DiscordWebhookInteractionAPI : IDiscordRestInteractionAPI
     (
         Snowflake applicationID,
         string token,
-        Optional<string?> content = new Optional<string?>(),
-        Optional<IReadOnlyList<IEmbed>?> embeds = new Optional<IReadOnlyList<IEmbed>?>(),
-        Optional<IAllowedMentions?> allowedMentions = new Optional<IAllowedMentions?>(),
-        Optional<IReadOnlyList<IMessageComponent>?> components = new Optional<IReadOnlyList<IMessageComponent>?>(),
-        Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>?> attachments = new Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>?>(),
+        Optional<string?> content = default,
+        Optional<IReadOnlyList<IEmbed>?> embeds = default,
+        Optional<IAllowedMentions?> allowedMentions = default,
+        Optional<IReadOnlyList<IMessageComponent>?> components = default,
+        Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>?> attachments = default,
         CancellationToken ct = default
     )
         => _underlying.EditOriginalInteractionResponseAsync(applicationID, token, content, embeds, allowedMentions, components, attachments, ct);
@@ -93,25 +98,72 @@ public class DiscordWebhookInteractionAPI : IDiscordRestInteractionAPI
 
     /// <inheritdoc />
     [ExcludeFromCodeCoverage]
-    public Task<Result<IMessage>> CreateFollowupMessageAsync
+    public async Task<Result<IMessage>> CreateFollowupMessageAsync
     (
         Snowflake applicationID,
         string token,
-        Optional<string> content = new Optional<string>(),
-        Optional<bool> isTTS = new Optional<bool>(),
-        Optional<IReadOnlyList<IEmbed>> embeds = new Optional<IReadOnlyList<IEmbed>>(),
-        Optional<IAllowedMentions> allowedMentions = new Optional<IAllowedMentions>(),
-        Optional<IReadOnlyList<IMessageComponent>> components = new Optional<IReadOnlyList<IMessageComponent>>(),
-        Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>> attachments = new Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>>(),
-        Optional<MessageFlags> flags = new Optional<MessageFlags>(),
+        Optional<string> content = default,
+        Optional<bool> isTTS = default,
+        Optional<IReadOnlyList<IEmbed>> embeds = default,
+        Optional<IAllowedMentions> allowedMentions = default,
+        Optional<IReadOnlyList<IMessageComponent>> components = default,
+        Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>> attachments = default,
+        Optional<MessageFlags> flags = default,
         CancellationToken ct = default
     )
-        => _underlying.CreateFollowupMessageAsync(applicationID, token, content, isTTS, embeds, allowedMentions, components, attachments, flags, ct);
+    {
+        // Hack! Use the presence of a context to determine that this is the initial response,
+        // and thus, can be returned as a response, instead of having to make an API call!
+        if (_contextInjector is not { Context: InteractionContext { HasRespondedToInteraction: false } })
+        {
+            return await _underlying.CreateFollowupMessageAsync(applicationID, token, content, isTTS, embeds, allowedMentions, components, attachments, flags, ct);
+        }
+
+        var interactionResult = await _dataService.TryGetLeaseAsync(token, ct);
+
+        if (!interactionResult.IsSuccess)
+        {
+            return Result<IMessage>.FromError(interactionResult);
+        }
+
+        await using var interaction = interactionResult.Entity;
+
+        IInteractionResponse adhocResponse = new InteractionResponse
+        (
+            InteractionCallbackType.ChannelMessageWithSource,
+            new
+            (
+                new InteractionMessageCallbackData
+                (
+                    Content: content,
+                    IsTTS: isTTS,
+                    Embeds: embeds,
+                    AllowedMentions: allowedMentions,
+                    Components: components,
+                    Flags: flags
+                )
+            )
+        );
+
+        interaction.MarkForDeletion();
+        interaction.Data.ResponseTCS.SetResult(new InteractionWebhookResponseData(adhocResponse, attachments));
+
+        // Why?, you may be asking. To this I answer that this is a cursed optimization to make responses faster.
+        // By releasing the TCS, we respond to the interaction via HTTP, which is faster than opening a new connection
+        // to Discord when using the /callback endpoint; however this would technically break the return of this method
+        // for any callers that rely on the return value. So, at the expense of holding the caller slightly longer
+        // (sending the response is asynchronous) to grab the response, we can return the same message that would be
+        // returned by the API call.
+        //
+        // Though, all of this only even matters if this method is called instead of CreateInteractionResponse, which
+        // is technically undocumented-but-supported behavior. 💀
+        return await _underlying.GetOriginalInteractionResponseAsync(applicationID, token, ct);
+    }
 
     /// <inheritdoc />
     [ExcludeFromCodeCoverage]
-    public Task<Result<IMessage>> GetFollowupMessageAsync(Snowflake applicationID, string token, Snowflake messageID, CancellationToken ct = default) => 
-        _underlying.GetFollowupMessageAsync(applicationID, token, messageID, ct);
+    public Task<Result<IMessage>> GetFollowupMessageAsync(Snowflake applicationID, string token, Snowflake messageID, CancellationToken ct = default) 
+        => _underlying.GetFollowupMessageAsync(applicationID, token, messageID, ct);
 
     /// <inheritdoc />
     [ExcludeFromCodeCoverage]
@@ -120,17 +172,16 @@ public class DiscordWebhookInteractionAPI : IDiscordRestInteractionAPI
         Snowflake applicationID,
         string token,
         Snowflake messageID,
-        Optional<string?> content = new Optional<string?>(),
-        Optional<IReadOnlyList<IEmbed>?> embeds = new Optional<IReadOnlyList<IEmbed>?>(),
-        Optional<IAllowedMentions?> allowedMentions = new Optional<IAllowedMentions?>(),
-        Optional<IReadOnlyList<IMessageComponent>?> components = new Optional<IReadOnlyList<IMessageComponent>?>(),
-        Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>?> attachments = new Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>?>(),
+        Optional<string?> content = default,
+        Optional<IReadOnlyList<IEmbed>?> embeds = default,
+        Optional<IAllowedMentions?> allowedMentions = default,
+        Optional<IReadOnlyList<IMessageComponent>?> components = default,
+        Optional<IReadOnlyList<OneOf<FileData, IPartialAttachment>>?> attachments = default,
         CancellationToken ct = default
-    )
-        => _underlying.EditFollowupMessageAsync(applicationID, token, messageID, content, embeds, allowedMentions, components, attachments, ct);
+    ) => _underlying.EditFollowupMessageAsync(applicationID, token, messageID, content, embeds, allowedMentions, components, attachments, ct);
 
     /// <inheritdoc />
     [ExcludeFromCodeCoverage]
-    public Task<Result> DeleteFollowupMessageAsync(Snowflake applicationID, string token, Snowflake messageID, CancellationToken ct = default) => 
-        _underlying.DeleteFollowupMessageAsync(applicationID, token, messageID, ct);
+    public Task<Result> DeleteFollowupMessageAsync(Snowflake applicationID, string token, Snowflake messageID, CancellationToken ct = default) 
+        => _underlying.DeleteFollowupMessageAsync(applicationID, token, messageID, ct);
 }
